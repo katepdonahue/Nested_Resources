3ff0a7b48704318040a6846119d832157f9048c4
Kate <katepdonahue@gmail.com>
2014-01-13 09:16:44 -0500

    Initial rails with two scaffolds, then I nested them myself...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NestedResources::Application.config.secret_token = '40383512f4f99ac797d41baef3109b1121428400274fe943137b1526ad78be4313e90f193d27a2b101c65f3b83679754daf6cd6801d5d978c012bf4560c1c950'
